01b146beaa66e08fe6cb5095105e5dd8ee18afc7
amar tech <amartech43@gmail.com>
2025-03-13 20:57:40 +0100

    installed sentry for performance

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,38 @@
-import { StrictMode } from 'react'
+import { React, StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://8910a9250de7f9df50b6d31b496d2b0f@o4508966855835648.ingest.de.sentry.io/4508966860357712",
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: useEffect,
+    }),
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: useEffect,
+    }),
+    Sentry.replayIntegration({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+    
+  ],
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ 
-   
-    react()
-  ],
+  plugins: [react(), sentryVitePlugin({
+    org: "amar-5e",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
